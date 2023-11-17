3e18a4ed185f9f775c2cbc53afef3f47ece09c21
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-11-17 19:21:32 +0000

    push code base

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>what do you need for your trip? 😍</h3>
+      <select
+        onChange={(e) => setQuantity(Number(e.target.value))}
+        value={quantity}
+      >
+        {Array.from({ length: 20 }, (current, index) => index + 1).map(
+          (num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          )
+        )}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
---
+++ src/components/Item.js
@@ -0,0 +1,18 @@
+export default function Item({
+  id,
+  description,
+  quantity,
+  packed,
+  onDeleteItem,
+  onToggleItem,
+}) {
+  return (
+    <li>
+      <input type="checkbox" value={packed} onChange={() => onToggleItem(id)} />
+      <span style={packed ? { textDecoration: "line-through" } : {}}>
+        {quantity} {description}
+      </span>
+      <button onClick={() => onDeleteItem(id)}>❌</button>
+    </li>
+  );
+}
